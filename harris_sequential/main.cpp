99a89bc202a68d640ac6a0caab2d810de30ba46c
Nathan <nathan.chong@gmail.com>
2011-10-23 13:39:03 +0100

    Make using icc.

--- harris_sequential/main.cpp
+++ harris_sequential/main.cpp
@@ -1,7 +1,7 @@
 #include "framework.h"
 #include "seq_scan.h"
 
-void run(int *data, int n, int num_iter, map<string,float> &timings) {
+void run(int *data, int n, int num_iter, map<string,float> &/* timings unused*/) {
   int *copy = new int[n];
   for (int run=0; run<num_iter; run++) {
     memcpy(copy, data, sizeof(int)*n);
